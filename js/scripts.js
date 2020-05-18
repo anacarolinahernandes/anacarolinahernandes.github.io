e9fd99767b8f100e8c2c0ae71f73854c2eaea892
Ana Carolina Hernandes <anacbhernandes@gmail.com>
2020-05-18 18:01:54 -0300

    ajustando as pastas e acrescentando funções

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,13 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
